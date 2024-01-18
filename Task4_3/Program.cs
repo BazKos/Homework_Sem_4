3d9e49e151caa181c6a7a4be05cb50ed4dbb38e9
Konstantin <konstbaz@gmail.com>
2024-01-19 02:01:38 +0300

    Решил 3-ю задачу ДЗ от 14.01.2024. Функции.

---
+++ Task4_3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая перевернёт одномерный массив
+//(первый элемент станет последним, второй – предпоследним и т.д.)
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+        int size = array.Length;
+        int k = array.Length / 2;
+        int temp = 0;
+        for ( int i = 0; i < k; i++)
+        {
+            temp = array [size - i - 1];
+            array [size - i - 1] = array[i];
+            array[i] = temp;
+        }
+        foreach (int e in array)
+        {
+            Console.Write($"[{e}] ");
+        }
+    }
+}
